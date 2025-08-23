379e32655e1939d8dca2bdba60df17945cfb4ca4
Mark Sadiki <msadiki06@icloud.com>
2025-08-22 21:22:13 -0500

    Fix linker issues
    Turns out we don't need to link anything... my bad...

--- build.rs
+++ build.rs
@@ -4,10 +4,15 @@ fn main() {
     #[cfg(target_vendor = "apple")]
     {
         println!("cargo:rerun-if-changed=src/header.h");
-        println!("cargo:rustc-link-lib=XPC");
 
         let bindings = bindgen::Builder::default()
             .header("src/header.h")
+            .allowlist_var("xpc.*")
+            .allowlist_var("_xpc.*")
+            .allowlist_var("XPC.*")
+            .allowlist_type("uuid_t")
+            .allowlist_function("dispatch_queue_create")
+            .allowlist_function("xpc.*")
             .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
             .generate()
             .expect("Unable to generate bindings");
